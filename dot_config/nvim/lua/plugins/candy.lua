5096b18d6f1923e7fe4700671967d4a020da7ad3
qweenkie <qweenkie.xd@gmail.com>
2026-01-02 19:18:24 +0200

    Added gruvbox.nvim

--- dot_config/nvim/lua/plugins/candy.lua
+++ dot_config/nvim/lua/plugins/candy.lua
@@ -4,8 +4,16 @@ return {
 		name = "cattpuccin",
 		priority = 1000,
 
+		-- config = function()
+		-- 	vim.cmd.colorscheme("catppuccin-mocha")
+		-- end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+
 		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 	{
